3742239824f4f549f37c17e53d9fc279edd11f45
KrisYan <krisyan3318@gmail.com>
2023-01-02 01:28:35 +0800

    資料排序按鈕 2

--- index.js
+++ index.js
@@ -47,39 +47,83 @@ app.get('/json-sales', (req, res) => {
   res.render('json-sales', {dataSorted});
 });
 
+// 回應排序資料 meth1--
+// app.get('/json-sales2', (req, res) => {
+//   const data = require(__dirname + '/data/sales.json');
+
+//   const {orderby} = req.query;
+//   // --req.query 已將 query string 轉為物件
+
+//   const handleObj = {
+//     name_asc: {
+//       label : '姓名由小到大',
+//       sort : (a, b) => {
+
+//       },
+//       selected : false
+//     },
+//     name_desc: {
+//       label : '姓名由大到小',
+//       sort : (a, b) => {
+
+//       },
+//       selected : false
+//     },
+//     age_asc: {
+//       label : '年齡由小到大',
+//       sort : (a, b) => {
+
+//       },
+//       selected : false
+//     },
+//     age_desc: {
+//       label : '年齡由大到小',
+//       sort : (a, b) => {
+
+//       },
+//       selected : false
+//     }
+//   };
+
+//   if (orderby){
+//     handleObj[orderby].selected = true;
+//   }
+
+//   res.render('json-sales2', {data, handleObj});
+// });
+
+// 回應排序資料 meth2--
 app.get('/json-sales2', (req, res) => {
   const data = require(__dirname + '/data/sales.json');
+
+  const {orderby} = req.query;
+  // --req.query 已將 query string 轉為物件
 
   const handleObj = {
     name_asc: {
       label : '姓名由小到大',
-      sort : (a, b) => {
-
-      }
+      sort : (a, b) => (a.name > b.name ? 1 : -1)
     },
     name_desc: {
       label : '姓名由大到小',
-      sort : (a, b) => {
-
-      }
+      sort : (a, b) => (a.name > b.name ? -1 : 1)
     },
     age_asc: {
       label : '年齡由小到大',
-      sort : (a, b) => {
-
-      }
+      sort : (a, b) => (a.age - b.age)
     },
     age_desc: {
       label : '年齡由大到小',
-      sort : (a, b) => {
-
-      }
+      sort : (a, b) => (b.age - a.age)
     }
   };
 
-  
-  
-  res.render('json-sales2', {data, handleObj});
+  // query string 有對應到 才排序--
+  if (handleObj[orderby]){
+    data.sort(handleObj[orderby].sort);
+  }
+
+  res.render('json-sales2', {data, handleObj, orderby});
 });
 
 app.get('/try-qs', (req, res) => {
