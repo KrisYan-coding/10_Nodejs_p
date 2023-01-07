9e0fe8466834fc17c5307a907107a747bde07f7a
KrisYan <krisyan3318@gmail.com>
2023-01-07 23:19:20 +0800

    address-book

---
+++ routes/address-book.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const db = require(__dirname + '/../modules/connect-mysql');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const [rows] = await db.query("SELECT * FROM products LIMIT 5");
+  res.json(rows);
+});
+
+module.exports = router;
